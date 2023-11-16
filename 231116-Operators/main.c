daf9455678045d0acd9bd7b020425cb8e61faba8
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-16 10:00:24 +0700

    first commit

---
+++ 231116-Operators/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+int main()
+{
+    int a = 12;
+    int b = 20;
+    _Bool result;
+
+    result = a && b;
+
+    printf("\n%d\n", result);
+
+    return 0;
+}
