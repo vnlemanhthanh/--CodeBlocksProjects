b137ce5e6c7562b56ea1370f15ac2737d23c8590
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-16 12:45:42 +0700

    sizeof

--- 231116-Bitwise/main.c
+++ 231116-Bitwise/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    unsigned int a = 60;    // 0011 1100
+    unsigned int a = 60;    // 0000 0000 0000 0000 0000 0000 0011 1100
     unsigned int b = 13;    // 0000 1101
     int c,d,e,f;
 
---
+++ SizeOf/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\nChar: %zd", sizeof(char));
+    printf("\nshort: %zd", sizeof(short));
+    printf("\nint: %zd", sizeof(int));
+    printf("\nlong: %zd", sizeof(long));
+    printf("\nlong long: %zd", sizeof(long long));
+    printf("\nfloat: %zd", sizeof(float));
+    printf("\ndouble: %zd", sizeof(double));
+    printf("\nlong double: %zd", sizeof(long double));
+    printf("\nunsigned int: %zd", sizeof(unsigned int));
+    printf("\nChar: %zd", sizeof());
+    printf("\nChar: %zd", sizeof(char));
+
+    return 0;
+}
